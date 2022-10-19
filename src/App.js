821ba9fab1e29c65189b835dfbe51553f42ea7b1
WiecznyX <bmunkhtushig00@gmail.com>
2022-10-19 23:30:35 +0800

    todoListDone!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+  function App() {
+    
+    const [inputValue , setInputValue] = useState("");
+    const [bichver , setTexts] = useState([]);
+
+    function clicker() {
+      setTexts([...bichver, inputValue]);  
+    }
+    function deleteTask() {
+      const newBichver = bichver.filter((_, index) => index !== 0);
+      setTexts(newBichver);
+    }
+    
+    const  AddTasks = (props) => {
+      return (
+        <div className="taskLists">
+          <div className="leftSide">
+            <input type="checkbox" className='jagsaalt'/>
+            <span className="tasks">{props.text}</span>
+          </div>
+          <button className="trash" onClick={deleteTask}>X</button>
+        </div>
+      )
+    }
+   
+    return (
+      <div className="container">
+        <h1>To-do list</h1>
+        <div className="newTaskMaker">
+          <input type="text" placeholder="New Task" onChange={(e) => {
+            setInputValue(e.target.value);
+          }}/>
+          <button onClick={clicker}>Add Tasks</button>
+        </div>
+        <div className='listContainer'>
+          {bichver.map((bichvers) => {
+            return <AddTasks key={bichvers + 1} text={bichvers} />
+          })}
+        </div>
+      </div>
+    )
+  }
 
 export default App;
